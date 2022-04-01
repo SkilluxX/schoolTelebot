7593126e5388d86b1736ad5b595835d4f84eb5de
ihatesummerr <ihatesummerr@yahoo.com>
2022-04-01 20:10:04 +0500

    import telebot api

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-print("Hello world!")
+import telebot
+import config
 
-print("!")
+bot = telebot.TeleBot(config.API_KEY)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Добро пожаловать в онлайн-школу!")
+
+@bot.message_handler(content_types = ['text'])
+def handle_message(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет')
+
+bot.infinity_polling()
